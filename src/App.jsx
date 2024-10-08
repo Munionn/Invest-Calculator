7f692a4c201470f0b276e8702a72c69216c14402
Mike <misasa0404gmail.com>
2024-10-08 18:16:15 +0300

    start of investment calculator

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header.jsx"
+import InputInfo from "./components/InputInfo.jsx"
+function App() {
+  return (
+    <>
+    <Header></Header>
+    <InputInfo></InputInfo>
+    </>
+    
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import logo from "../assets/investment-calculator-logo.png"
+export default function Header(){
+  return (
+  <header id="header">
+    <img src={logo} alt="Picture of investment calculator"/>
+    <h1>Inevetment Calculator</h1>
+  </header>
+  )
+};
